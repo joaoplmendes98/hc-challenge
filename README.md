a1d6f2cc1391a8b9e46f006cd82d10fc29fd784f
João Mendes <joaoplmendes98@gmail.com>
2024-11-06 12:10:24 +0000

    convert date to actual time if < 24h

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ npm run dev
 
 ### Flow
 The main page is the users page, all the actions (create, edit, delete) are available from the table.
-Once the user name or the icon of the row is clicked it takes you to the user orders page and table.
+Once the user name or the icon of the row is clicked it takes you to the user orders page where the specific table and actions are available.
--- src/assets/helpers/dateHandler.ts
+++ src/assets/helpers/dateHandler.ts
@@ -5,10 +5,26 @@ const convertDate = (value: string) => {
 
 const convertDateWithTime = (value: string) => {
     const date = new Date(value)
+    const now = new Date()
+    const diff = now.getTime() - date.getTime()
 
     const hours = date.getHours().toString().padStart(2, '0')
     const minutes = date.getMinutes().toString().padStart(2, '0')
     const seconds = date.getSeconds().toString().padStart(2, '0')
+
+    if (diff < 24 * 60 * 60 * 1000) {
+        const diffSeconds = Math.floor(diff / 1000)
+        const diffMinutes = Math.floor(diff / (1000 * 60))
+        const diffHours = Math.floor(diff / (1000 * 60 * 60))
+
+        if (diffSeconds < 60) {
+            return `${diffSeconds} second${diffSeconds !== 1 ? 's' : ''} ago`
+        } else if (diffMinutes < 60) {
+            return `${diffMinutes} minute${diffMinutes !== 1 ? 's' : ''} ago`
+        } else {
+            return `${diffHours} hour${diffHours !== 1 ? 's' : ''} ago`
+        }
+    }
 
     return `${date.toISOString().slice(0, 10)} ${hours}:${minutes}:${seconds}`
 }
